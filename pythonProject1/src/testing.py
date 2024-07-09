8ec3f044a67def835a19943aec4097daed03402f
Emily Zhu <ezhu0922@gmail.com>
2024-07-09 13:30:23 -0400

    parser isnt working, but want to save

--- pythonProject1/src/testing.py
+++ pythonProject1/src/testing.py
@@ -1,79 +1,6 @@
-"""
- * Class:       CS 4308 Section W01
- * Term:        Summer 2024
- * Name:        Emily Zhu
- * Instructor:  Sharon Perry
- * Project:     Deliverable P1 Scanner
-"""
+from tokenize import tokenize
 
-import re
 
-# function
-code = """function b()
-   y = 2 + 3
-   if y == 5 then
-       print(10)
-   else
-       print(1)
-   end
-end
-"""
-
-# token patterns for the lexical analyzer
-token_patterns = [
-    ('FUNCTION', r'\bfunction\b'),
-    ('ID', r'[a-zA-Z]\w*'),
-    ('LITERAL_INTEGER', r'\d+'),
-    ('ASSIGNMENT_OPERATOR', r'='),
-    ('LE_OPERATOR', r'<='),
-    ('LT_OPERATOR', r'<'),
-    ('GE_OPERATOR', r'>='),
-    ('GT_OPERATOR', r'>'),
-    ('EQ_OPERATOR', r'=='),
-    ('NE_OPERATOR', r'~='),
-    ('ADD_OPERATOR', r'\+'),
-    ('SUB_OPERATOR', r'-'),
-    ('MUL_OPERATOR', r'\*'),
-    ('DIV_OPERATOR', r'/'),
-    ('IF', r'\bif\b'),
-    ('ELSE', r'\belse\b'),
-    ('THEN', r'\bthen\b'),
-    ('END', r'\bend\b'),
-    ('PRINT', r'\bprint\b'),
-    ('OPEN_PARENTHESIS', r'\('),
-    ('CLOSE_PARENTHESIS', r'\)'),
-    ('WHITESPACE', r'\s+'),
-    ('COMMENT', r'//.*'),
-]
-
-token_regex = '|'.join(f'(?P<{name}>{pattern})' for name, pattern in token_patterns)
-token_re = re.compile(token_regex)
-
-def tokenize(code):
-    tokens = []
-    for match in token_re.finditer(code):
-        token_type = match.lastgroup
-        token_value = match.group(token_type)
-        if token_type != 'WHITESPACE' and token_type != 'COMMENT':  # Ignore whitespace and comments
-            tokens.append((token_type, token_value))
-    return tokens
-
-# print statements to make code look organized
-# this is the test input
-print(f'{"-" * 40}')
-print("Test Input:")
-print(f'{"-" * 40}')
-print(code)
-
-# this is for the lexeme and symbol
-print(f'{"Lexeme":<15} {"Symbol":<10}')
-print(f'{"-" * 15} {"-" * 20}')
-
-tokens = tokenize(code)
-for token in tokens:
-    print(f'{token[1]:<15} {token[0]}')
-
-# PARSER
 class Parser:
     def __init__(self, tokens):
         self.tokens = tokens
@@ -166,7 +93,6 @@ class Parser:
             self.expression()
             self.eat('CLOSE_PARENTHESIS')
 
-
 # Test the parser
 code1 = """function b()
    y = 2 + 3
@@ -196,5 +122,3 @@ for code in codes:
     parser = Parser(tokens)
     parser.program()
     print()
-
-
